8e646d2c77c3d056a16e0b24a65da189c1ff226e
Justine Erin Cuenco <jem.cuenco@gmail.com>
2017-10-10 21:51:19 +0800

    Created homepage

---
+++ src/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Image } from "semantic-ui-react";
+
+const Banner = () =>
+ (
+    <Image src='../images/banner2.JPG' position='relative' width='100%'/>
+ )
+
+ export default Banner;
