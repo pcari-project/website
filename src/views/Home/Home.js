27250a2202da3e7a4e89238ce7b217d707dbf010
Justine Erin Cuenco <jem.cuenco@gmail.com>
2017-10-11 11:32:36 +0800

    Added react router to the application

---
+++ src/views/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './Banner';
+import ActivitiesSection from './ActivitiesSection';
+
+const Home = () => (
+    <div>
+        <Banner/>
+        <ActivitiesSection/>
+    </div>
+)
+
+export default Home;
