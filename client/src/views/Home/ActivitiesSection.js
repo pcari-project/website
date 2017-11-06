1487b0cb850fcf167bf8e03c06d83f4039874804
Justine Erin Cuenco <jem.cuenco@gmail.com>
2017-11-06 10:11:27 +0800

    Added pagination in the activities section in the homepage

--- client/src/views/Home/ActivitiesSection.js
+++ client/src/views/Home/ActivitiesSection.js
@@ -1,84 +1,128 @@
-import React from 'react';
-import { Container, Grid, Image, Card, Header, Icon } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Container, Grid, Image, Card, Header, Icon, Menu } from 'semantic-ui-react'
 
-const ActivitiesSection = () =>
-(
-    <Container style={{padding: '1.75em'}}>
-        <Header as='h2' textAlign='center'>Recent Activities</Header>
-        <Grid doubling stackable columns={4}>
-            <Grid.Column>
-                <Card>
-                    <a><Image src='../images/Activity1.jpg' /></a>
-                    <Card.Content>
-                    <Card.Header>
-                        General Meeting
-                    </Card.Header>
-                    <Card.Description>
-                        General meeting on June 6 at 11AM at the HL Conference Room about urgent matters
-                    </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <Icon name='calendar' />
-                        June 6, 2017
-                    </Card.Content>
-                </Card>
-            </Grid.Column>
-            
-            <Grid.Column>
-                <Card>
-                    <a><Image src='../images/Activity2.jpg' /></a>
-                    <Card.Content>
-                    <Card.Header>
-                        General Meeting
-                    </Card.Header>
-                    <Card.Description>
-                        Project meeting on June 23 at 11AM for the preparation for the visit of Dr. Glaser and his postdoc, workplans and other matters
-                    </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <Icon name='calendar' />
-                        June 23, 2017
-                    </Card.Content>
-                </Card>
-            </Grid.Column>
+import axios from 'axios';
 
-            <Grid.Column>
-                <Card>
-                    <a><Image src='../images/Activity3.jpg' /></a>
-                    <Card.Content>
-                    <Card.Header>
-                        General Meeting
-                    </Card.Header>
-                    <Card.Description>
-                        General Meeting on June 30 at 10AM for the presentation of workplans by Research Fellows at CEAT HL Conference Room
-                    </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <Icon name='calendar' />
-                        June 30, 2017
-                    </Card.Content>
-                </Card>
-            </Grid.Column>
+class ActivitiesSection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            activeItem: 1,
+            itemPerPage: 4
+        };
+        this.fetch = this.fetch.bind(this);
+        this.handleItemClick = this.handleItemClick.bind(this);
+        this.handleItemClickRight = this.handleItemClickRight.bind(this);
+        this.handleItemClickLeft = this.handleItemClickLeft.bind(this);
+    }
 
-            <Grid.Column>
-                <Card>
-                    <a><Image src='../images/Activity4.jpg' /></a>
-                    <Card.Content>
-                    <Card.Header>
-                        Arrival of Drs. Steven Glaser and Carlos Oroza
-                    </Card.Header>
-                    <Card.Description>
-                        Arrival of Drs. Steven Glaser and Carlos Oroza
-                    </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <Icon name='calendar' />
-                        July 9, 2017
-                    </Card.Content>
-                </Card>
-            </Grid.Column>
-        </Grid>
-    </Container>
-)
+    fetch(serverUrl){
+        axios.get(`${serverUrl}?filter={"where":{"active":true}}`)
+        .then(res => {
+            this.setState({ data: res.data });
+        })
+        .catch(err => {
+            alert('No data returned from the server!');
+            console.error(err);
+        });
+    }
+
+    handleItemClick(e, {name}){
+        name = parseInt(name, 10);
+        this.setState({ activeItem: name });
+    }
+
+    handleItemClickRight(e){
+        const pageNumbers = [];
+        const { itemPerPage } = this.state;
+        for (let i = 1; i <= Math.ceil(this.state.data.length / itemPerPage); i++) {
+            pageNumbers.push(i);
+        }
+        let item = this.state.activeItem + 1;
+        if (this.state.activeItem === pageNumbers.length){
+            item = this.state.activeItem;
+        }
+        this.setState({ activeItem: item });
+    }
+
+    handleItemClickLeft(e){
+        let item = this.state.activeItem - 1;
+        if (this.state.activeItem === 1){
+            item = this.state.activeItem;
+        }
+        this.setState({ activeItem: item });
+    }
+
+    componentDidMount() {
+        const table = 'activities';
+        const serverUrl = 'http://localhost:3100/api/' + table;
+        this.fetch(serverUrl);
+    }
+
+    render (){
+        const { activeItem, itemPerPage } = this.state;
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(this.state.data.length / itemPerPage); i++) {
+          pageNumbers.push(i);
+        }
+        const renderPageNumbers = pageNumbers.map(number => {
+            return (
+                <Menu.Item
+                key={number}
+                name={''+number+''} // eslint-disable-next-line
+                active={activeItem == number}
+                onClick={this.handleItemClick}>
+                {number}
+                </Menu.Item>
+            );
+        });
+        const indexOfLast = activeItem * itemPerPage;
+        const indexOfFirst = indexOfLast - itemPerPage;
+        let currentData = this.state.data.slice(indexOfFirst, indexOfLast);
+        
+        let item = currentData.map(activity => {
+            const thumbnail = activity.thumbnail;
+            return (
+                <Grid.Column>
+                    <Card>
+                        <a><Image src={thumbnail} /></a>
+                        <Card.Content>
+                        <Card.Header>
+                            {activity.name}
+                        </Card.Header>
+                        <Card.Description>
+                            {activity.description}
+                        </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <Icon name='calendar' />
+                            {activity.created}
+                        </Card.Content>
+                    </Card>
+                </Grid.Column>
+            )
+        });
+
+        return (
+            <Container style={{padding: '1.75em'}}>
+                <Header as='h2' textAlign='center'>Recent Activities</Header>
+                <Grid doubling stackable columns={4}>
+                    {item}
+                </Grid>
+
+                <Menu pagination style={{margin: '0.7em'}}>
+                <Menu.Item as='a' onClick={this.handleItemClickLeft} icon>
+                    <Icon name='left chevron' />
+                </Menu.Item>
+                {renderPageNumbers}
+                <Menu.Item  as='a' onClick={this.handleItemClickRight} icon>
+                    <Icon name='right chevron' />
+                </Menu.Item>
+                </Menu>
+            </Container>
+        );
+    }
+}
 
 export default ActivitiesSection;
