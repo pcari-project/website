f2d43a4447125cb48f0113d57e5aff412cbc5b31
Justine Erin Cuenco <jem.cuenco@gmail.com>
2017-11-06 20:08:18 +0800

    Created individual pages for each activity

--- client/src/Main.js
+++ client/src/Main.js
@@ -5,6 +5,7 @@ import Home from './views/Home/Home';
 import About from './views/About/About';
 import Team from './views/Team/Team';
 import Activities from './views/Activities/Activities';
+import ActivitiesPhotos from './views/Activities/ActivitiesPhotos';
 import Contact from './views/Contact/Contact';
 
 class Main extends Component {
@@ -15,6 +16,7 @@ class Main extends Component {
         <Route path="/about" component={About} />
         <Route path="/team" component={Team} />
         <Route path="/activities" component={Activities} />
+        <Route path="/activities-photos/:id" component={ActivitiesPhotos} />
         <Route path="/contact" component={Contact} />
       </Switch>
     )
