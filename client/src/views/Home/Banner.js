b63fa080a2e53651a2c7f4a21fbfd6823cf0c505
Justine Erin Cuenco <jem.cuenco@gmail.com>
2017-11-15 12:09:11 +0800

    Placed text with font scaling (based on the width of container) over banner image

--- client/src/views/Home/Banner.js
+++ client/src/views/Home/Banner.js
@@ -3,7 +3,12 @@ import { Image } from "semantic-ui-react";
 
 const Banner = () =>
  (
-    <Image src='../images/banner2.JPG' position='relative' width='100%'/>
+    <div style={{position: 'relative', textAlign:'center', color: 'rgb(37, 34, 34)'}}>
+        <Image src='images/banner2.JPG' width='100%'/>
+        <div style={{position: 'absolute', top: '30%', left: '50%', right: '2%'}}>
+            <p style={{fontSize:'2.5vw', fontWeight: 'bold', fontFamily: 'open sans'}}>Development of Wireless Sensor Network-Based Water Information System for Efficient Irrigation Water Management in the Philippines</p>
+        </div>
+    </div>
  )
 
  export default Banner;
