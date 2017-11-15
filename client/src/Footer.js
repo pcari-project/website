582dceea9bb4394c5fe844a7443149683b9266e0
Justine Erin Cuenco <jem.cuenco@gmail.com>
2017-11-15 17:15:04 +0800

    Added UPLB logo and UCB logo in the footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -7,7 +7,7 @@ const Footer = () =>
             <Container>
                 <Grid divided inverted stackable>
                     <Grid.Row>
-                        <Grid.Column width={4}>
+                        <Grid.Column width={3}>
                             <Header inverted as='h4' content='BROWSE' />
                             <List link inverted>
                                 <List.Item as='a' href='/'>Homepage</List.Item>
@@ -17,7 +17,7 @@ const Footer = () =>
                                 <List.Item as='a'>Live Data</List.Item>
                             </List>
                         </Grid.Column>
-                        <Grid.Column width={5}>
+                        <Grid.Column width={6}>
                             <Header inverted as='h4' content='CONTACT' />
                             <List>
                                 <List.Item icon='marker' content='CEAT Building A Room 110, LWRD, IAE, CEAT, University of the Philippines Los Banos (UPLB)' />
@@ -25,15 +25,18 @@ const Footer = () =>
                                 <List.Item icon='phone' content='(63-49)-536-2387' />
                             </List>
                         </Grid.Column>
-                        <Grid.Column width={4}>
+                        <Grid.Column width={3}>
                             <Header inverted as='h4' content='FOLLOW' />
                             <List>
                                 <List.Item icon='twitter' content={<a href='https://twitter.com/pcariproject'>@pcariproject</a>} />
                                 <List.Item icon='facebook f' content={<a href='https://facebook.com/pcariproject'>/pcariproject</a>} />
                             </List>
                         </Grid.Column>
-                        <Grid.Column width={3} textAlign='center'>
-                            <Image src='../images/CHED.png' size='tiny' centered />
+                        <Grid.Column width={4} textAlign='center'>
+                            {/* <Image src='../images/PCARI.png' width='200' title='Philippine-California Advanced Research Institutes' style={{display:'inline-block', margin:'0.2em'}} /><br/> */}
+                            <Image src='../images/CHED.png' width='60' title='Commission on Higher Education' style={{display:'inline-block', margin:'0.2em'}} />
+                            <Image src='../images/UPLB.png' width='70' title='University of the Philippines Los Baños' style={{display:'inline-block', margin:'0.2em'}} />
+                            <Image src='../images/UCB.png' width='65' title='University of California, Berkeley' style={{display:'inline-block', margin:'0.2em'}} /><br/>
                             <Icon name='copyright' style={{marginTop:'0.5em'}}/> 2017
                         </Grid.Column>
                     </Grid.Row>
