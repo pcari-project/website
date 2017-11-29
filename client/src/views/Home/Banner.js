0e2323fc6c376f6ca1ee95d164b5795796c652fc
Justine Erin Cuenco <jem.cuenco@gmail.com>
2017-11-29 10:42:28 +0800

    Modified banner text

--- client/src/views/Home/Banner.js
+++ client/src/views/Home/Banner.js
@@ -8,6 +8,7 @@ const Banner = () =>
         <Image src='images/banner2.JPG' width='100%'/>
         <div className='banner-text-div'>
             <p className='banner-text'>Development of Wireless Sensor Network-Based Water Information System for Efficient Irrigation Water Management in the Philippines</p>
+            <p className='banner-text'>(PCARI WISEIr Project)</p>
         </div>
     </div>
  )
